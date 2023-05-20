a0fce55bb29c9da8e95a8653051e1381472969c5
GabrielMeloBatista <gabrielmelobatisa@hotmail.com>
2023-05-19 22:40:25 -0300

    Adicionado Pagina de Ajuda

--- src/app/pages/anime/anime-routing.module.ts
+++ src/app/pages/anime/anime-routing.module.ts
@@ -21,6 +21,10 @@ export const animeRoutes: Routes = [
       {
         path: "novo",
         component: FormAnimeComponent
+      },
+      {
+        path: ":codigo",
+        component: FormAnimeComponent
       }
     ]
   },
